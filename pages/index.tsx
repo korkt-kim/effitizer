5e249512377f0f26a8b993678545825e56ca9e1a
Alan Choi <alan.choi@sendbird.com>
2022-05-07 14:39:01 -0700

    add footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import ArrowIcon from '../components/ArrowIcon';
 import PlusIcon from '../components/PlusIcon';
 import classNames from 'classnames';
 import { useInfiniteQuery } from 'react-query';
+import InstagramIcon from '../components/InstagramIcon';
 
 type Category = { id: number; name: string };
 type CategoryList = Category[];
@@ -59,6 +60,8 @@ async function fetchContents({
   );
   return response.json();
 }
+
+const emailAddress = 'interaction0318@naver.com';
 
 export const getServerSideProps: GetServerSideProps<Props> = async (
   context
@@ -199,16 +202,39 @@ const Home: NextPage<Props> = ({ categories, initialContentsResponse }) => {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+        <div className={styles.aboutEffitizer}>
+          <Image src="/logo.png" alt="로고" width={76} height={89} />
+          <p>
+            식전에 가볍게 먹는 에피타이저처럼 ‘쉽고 빠른 독서를 위한 숏폼 전자책
+            서비스’를 제공하고 있습니다.
+          </p>
+        </div>
+        <ul className={styles.footerLinks}>
+          <li>
+            <a>서비스 소개</a>
+          </li>
+          <li>
+            <a>개인정보처리방침</a>
+          </li>
+          <li>
+            <a>고객센터</a>
+          </li>
+          <li>
+            <a>이용약관</a>
+          </li>
+        </ul>
+        <div className={styles.companyInfo}>
+          <div>대표: 노상호 | 사업자등록번호 : 000-00-00000</div>
+          <div>
+            이메일 : <a href={'mailto:' + emailAddress}>{emailAddress}</a>
+          </div>
+        </div>
+        <a href="https://instagr.am/effitizer" target="_blank" rel="noreferrer">
+          <InstagramIcon aria-label="Instagram" />
         </a>
+        <small className={styles.copyright}>
+          Copyright © 2021 WASD Pte. Ltd. All rights reserved
+        </small>
       </footer>
     </div>
   );
